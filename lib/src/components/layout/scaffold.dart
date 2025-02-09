a4fddb9c5490171b593a0977fc7ced39b8fd2da6
ultrawideturbodev <brian@ultrawideturbodev.com>
2025-02-09 20:31:36 +0100

    [TBS] Remove focus traversalgroup

--- lib/src/components/layout/scaffold.dart
+++ lib/src/components/layout/scaffold.dart
@@ -222,86 +222,84 @@ class _AppBarState extends State<AppBar> {
     final barData = Data.maybeOf<ScaffoldBarData>(context);
     var surfaceBlur = widget.surfaceBlur ?? theme.surfaceBlur;
     var surfaceOpacity = widget.surfaceOpacity ?? theme.surfaceOpacity;
-    return FocusTraversalGroup(
-      child: ClipRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(
-            sigmaX: surfaceBlur ?? 0,
-            sigmaY: surfaceBlur ?? 0,
-          ),
-          child: Container(
-            color: widget.backgroundColor ??
-                theme.colorScheme.background.scaleAlpha(surfaceOpacity ?? 1),
-            alignment: widget.alignment,
-            padding: widget.padding ??
-                (const EdgeInsets.symmetric(
-                      horizontal: 18,
-                      vertical: 12,
-                    ) *
-                    scaling),
-            child: SafeArea(
-              top: widget.useSafeArea &&
-                  barData?.isHeader == true &&
-                  barData?.childIndex == 0,
-              right: widget.useSafeArea,
-              left: widget.useSafeArea,
-              bottom: widget.useSafeArea &&
-                  barData?.isHeader == false &&
-                  barData?.childIndex == (barData?.childrenCount ?? 0) - 1,
-              child: SizedBox(
-                height: widget.height,
-                child: IntrinsicHeight(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      if (widget.leading.isNotEmpty)
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: surfaceBlur ?? 0,
+          sigmaY: surfaceBlur ?? 0,
+        ),
+        child: Container(
+          color: widget.backgroundColor ??
+              theme.colorScheme.background.scaleAlpha(surfaceOpacity ?? 1),
+          alignment: widget.alignment,
+          padding: widget.padding ??
+              (const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 12,
+                  ) *
+                  scaling),
+          child: SafeArea(
+            top: widget.useSafeArea &&
+                barData?.isHeader == true &&
+                barData?.childIndex == 0,
+            right: widget.useSafeArea,
+            left: widget.useSafeArea,
+            bottom: widget.useSafeArea &&
+                barData?.isHeader == false &&
+                barData?.childIndex == (barData?.childrenCount ?? 0) - 1,
+            child: SizedBox(
+              height: widget.height,
+              child: IntrinsicHeight(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    if (widget.leading.isNotEmpty)
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: widget.leading,
+                      ).gap(widget.leadingGap ?? (4 * scaling)),
+                    Flexible(
+                      fit: widget.trailingExpanded
+                          ? FlexFit.loose
+                          : FlexFit.tight,
+                      child: widget.child ??
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              if (widget.header != null)
+                                KeyedSubtree(
+                                  key: const ValueKey('header'),
+                                  child: widget.header!.muted().small(),
+                                ),
+                              if (widget.title != null)
+                                KeyedSubtree(
+                                  key: const ValueKey('title'),
+                                  child: widget.title!.large().medium(),
+                                ),
+                              if (widget.subtitle != null)
+                                KeyedSubtree(
+                                  key: const ValueKey('subtitle'),
+                                  child: widget.subtitle!.muted().small(),
+                                ),
+                            ],
+                          ),
+                    ),
+                    if (widget.trailing.isNotEmpty)
+                      if (!widget.trailingExpanded)
                         Row(
                           crossAxisAlignment: CrossAxisAlignment.center,
-                          children: widget.leading,
-                        ).gap(widget.leadingGap ?? (4 * scaling)),
-                      Flexible(
-                        fit: widget.trailingExpanded
-                            ? FlexFit.loose
-                            : FlexFit.tight,
-                        child: widget.child ??
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                if (widget.header != null)
-                                  KeyedSubtree(
-                                    key: const ValueKey('header'),
-                                    child: widget.header!.muted().small(),
-                                  ),
-                                if (widget.title != null)
-                                  KeyedSubtree(
-                                    key: const ValueKey('title'),
-                                    child: widget.title!.large().medium(),
-                                  ),
-                                if (widget.subtitle != null)
-                                  KeyedSubtree(
-                                    key: const ValueKey('subtitle'),
-                                    child: widget.subtitle!.muted().small(),
-                                  ),
-                              ],
-                            ),
-                      ),
-                      if (widget.trailing.isNotEmpty)
-                        if (!widget.trailingExpanded)
-                          Row(
+                          children: widget.trailing,
+                        ).gap(widget.trailingGap ?? (4 * scaling))
+                      else
+                        Expanded(
+                          child: Row(
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: widget.trailing,
-                          ).gap(widget.trailingGap ?? (4 * scaling))
-                        else
-                          Expanded(
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: widget.trailing,
-                            ).gap(widget.trailingGap ?? (4 * scaling)),
-                          ),
-                    ],
-                  ).gap(18 * scaling),
-                ),
+                          ).gap(widget.trailingGap ?? (4 * scaling)),
+                        ),
+                  ],
+                ).gap(18 * scaling),
               ),
             ),
           ),
